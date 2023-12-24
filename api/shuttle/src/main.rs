c148e9c434c268d0552bfea456f02034fa784610
Nauris Tisancins <nauristisanchins@gmail.com>
2023-12-24 19:31:39 +0000

    add Shuttle.toml file

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,16 +1,28 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
+// This function is an Actix Web handler that responds with the string "Hello World!"
+//when accessed.
 #[get("/")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/users")]
+async fn get_users() -> &'static str {
+    "USERS!"
+}
+
+// It is used to define the main function for the Shuttle runtime. Shuttle is a library
+//for building concurrent, distributed, and parallel programs in Rust.
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
+        cfg.service(get_users);
     };
 
+    //Finally, the closure config is converted into a ShuttleActixWeb type using the
+    //into() method, and the result is wrapped in an Ok
     Ok(config.into())
 }
