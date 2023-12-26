5c4121b04d0b0bfc1c5b7ab51cd92e71f8252da4
Nauris Tisancins <nauristisanchins@gmail.com>
2023-12-26 18:10:25 +0000

    added instrumentation

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -7,6 +7,7 @@ use sqlx::Executor;
 //when accessed.
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
         .fetch_one(db.as_ref())
         .await;
