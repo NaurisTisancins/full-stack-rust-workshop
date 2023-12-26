12b953052e96f727cb0ad77fb746ab24cb4fd679
Nauris Tisancins <nauristisanchins@gmail.com>
2023-12-26 20:32:42 +0000

    use configure

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,6 +1,17 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse,
+};
 
-#[get("/health")]
+pub fn service(cfg: &mut ServiceConfig) {
+    cfg.route("/health", web::get().to(health));
+}
+//alternative when using #[get("/health")] macro
+// pub fn service(cfg: &mut ServiceConfig) {
+//     cfg.service(health);
+// }
+
+// #[get("/health")] // automatically adds pub keyword to the function
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
         .append_header(("version", "0.0.1"))
